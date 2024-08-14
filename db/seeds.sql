441344209b976a3c1ec9c283fb8765c8b04f8bd6
smrsun4 <smrsun4@gmail.com>
2024-08-13 22:20:55 -0400

    added starter code and folder structure

---
+++ db/seeds.sql
@@ -0,0 +1,11 @@
+const express = require('express');
+const { } = require('pg')
+
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
