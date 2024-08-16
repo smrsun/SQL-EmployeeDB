14e9c6d9a8a3d9d0417af2dedd5e7e2f6a5fd980
smrsun4 <smrsun4@gmail.com>
2024-08-15 22:25:09 -0400

    added database create tables in schema file

--- db/schema.sql
+++ db/schema.sql
@@ -1,2 +1,24 @@
 DROP DATABASE IF EXISTS employee_db
-CREATE DATABASE employee_db
+CREATE DATABASE employee_db;
+
+\c employee_db
+
+CREATE TABLE departments (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE roles (
+  id SERIAL PRIMARY KEY,
+  title VARCHAR(50) NOT NULL
+  salary DECIMAL NOT NULL,
+  departments_id INTEGER REFERENCES departments(id)
+);
+
+CREATE TABLE employees (
+  id SERIAL PRIMARY KEY,
+  first_name VARCHAR(50) NOT NULL,
+  last_name VARCHAR(50) NOT NULL,
+  role_id INTEGER REFERENCES roles(id)
+  manager_id INTEGER REFERENCES employees(id)
+);
