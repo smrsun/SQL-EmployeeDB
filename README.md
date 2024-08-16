6bc66372b018f31d34090e9912f8137f3fa60301
smrsun4 <smrsun4@gmail.com>
2024-08-15 22:56:03 -0400

    added readme and some begining code to the index.js for the initial prompt

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # SQL-EmployeeDB
+
+This is a command-line application for managing an employee database using Node.js, Inquirer, and PostgreSQL.
+
+## Installation
+
+Run `npm install` to install dependencies. Ensure PostgreSQL is running and the database is set up and run `node index.js` to start the application. Follow the prompts.
+
+## Features
+
+- View all departments
+- View all roles
+- View all employees
+- Add a department
+- Add a role
+- Add an employee
+- Update an employee role
+
+## Walkthrough Video
+
+[Link to Walkthrough Video]()
+
+## GitHub Repo Page
+[My GitHub](https://github.com/smrsun/SQL-EmployeeDB)
--- db/index.js
+++ db/index.js
@@ -1,3 +1,35 @@
 const inquirer = require('inquirer')
-const fs = require('fs')
+const {Client} = require('pg')
 
+const client = new Client ({
+    user:'username',
+    host: 'localhost',
+    database: 'employee_db',
+    password: 'password',
+    PORT: 3001,
+});
+
+client.connect();
+
+async function main() {
+    const { action } = await inquirer.prompt({
+        type: 'list',
+        name: 'action',
+        message: 'What would you like to do?',
+        choices: [
+            'View all departments',
+            'View all roles',
+            'View all employees',
+            'Add a department',
+            'Add a role',
+            'Add an employee',
+            'Update an employee role',
+        ],
+    });
+   
+    
+}
+
+async function viewAllDepartments() {
+   
+}
