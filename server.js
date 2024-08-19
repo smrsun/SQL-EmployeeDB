5ed031aa489aefa53f577b0610fa2e7f9f7f0c76
smrsun4 <smrsun4@gmail.com>
2024-08-19 13:01:11 -0400

    updated info in server.js file and changed db.js to queries.js

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require('express');
-const { Pool } = require('pg')
-
+const { Pool } = require('pg');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -8,3 +7,14 @@ const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+const pool = new Pool({
+    user: 'username',
+    password: 'password',
+    host:'localhost',
+    database: 'employee_db'
+   
+},
+console.log(`Connected to Employee Database`));
+
+module.exports = pool;
